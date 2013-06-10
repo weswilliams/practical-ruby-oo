a10eb51e9a7b060ddcd646086d7f7c4bbde833d7
Wes Williams <wes.williamz@gmail.com>
2013-06-10 08:15:03 -0700

    gear and wheel classes

---
+++ Gear.rb
@@ -0,0 +1,36 @@
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, wheel)
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
+  end
+
+  def ratio
+    @chainring / @cog.to_f
+  end
+  
+  def gear_inches
+    ratio * @wheel.diameter
+  end
+
+end
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
