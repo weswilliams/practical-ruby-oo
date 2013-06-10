8cb9a4cdaed9a0fe17e9325b4aae426c98ccf256
Wes Williams <wes.williamz@gmail.com>
2013-06-10 08:56:01 -0700

    removed order dependency on creating gear and added defaults

--- Gear.rb
+++ Gear.rb
@@ -1,10 +1,15 @@
 class Gear
   attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog, wheel)
-    @chainring = chainring
-    @cog = cog
-    @wheel = wheel
+  def initialize(args)
+    args = defaults.merge(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
+    @wheel = args[:wheel]
+  end
+
+  def defaults
+    {:chainring => 40, :cog => 18}
   end
 
   def ratio
