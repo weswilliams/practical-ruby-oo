74636740d3b16b134c1eafacfcebe528aa5148a4
Wes Williams <wes.williamz@gmail.com>
2013-06-17 19:13:58 -0700

    replace inheritance with composition

--- Bicycle.rb
+++ Bicycle.rb
@@ -1,84 +1,41 @@
-class Bicycle
-  attr_reader :size, :chain, :tire_size
+class Parts
+  attr_reader :parts
 
-  def initialize(args={})
-    @size = args[:size]
-    @chain = args[:chain] || default_chain
-    @tire_size = args[:tire_size] || default_tire_size
-    post_initialize(args)
+  def initialize(parts)
+    @parts = parts
   end
 
   def spares
-    { tire_size: tire_size,
-      chain: chain}.merge(local_spares)
-  end
-
-  def default_tire_size
-    raise NotImplementedError
-  end
-
-  def post_initialize(args)
-    nil
-  end
-
-  def local_spares
-    {}
-  end
-
-  def default_chain
-    '10-speed'
+    parts.select { |part| part.needs_spare }
   end
 end
 
-class RoadBike < Bicycle
-  attr_reader :tape_color
+class Part
+  attr_reader :name, :description, :needs_spare
 
-  def post_initialize(args)
-    @tape_color = args[:tape_color]
-  end
-
-  def local_spares
-    {tape_color: tape_color}
-  end
-
-  def default_tire_size
-    '23'
+  def initialize(args)
+    @name = args[:name]
+    @descriptiion = args[:description]
+    @needs_spare = args.fetch(:needs_spare, true)
   end
 end
 
-class MountainBike < Bicycle
-  attr_reader :front_shock, :rear_shock
+class Bicycle
+  attr_reader :size, :parts
 
-  def post_initialize(args)
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
+  def initialize(args={})
+    @size = args[:size]
+    @parts = args[:parts]
   end
 
-  def local_spares
-    {rear_shock: rear_shock}
-  end
-
-  def default_tire_size
-    '2.1'
+  def spares
+    parts.spares
   end
 end
 
-class RecumbentBike < Bicycle
-  attr_reader :flag
-
-  def post_initialize(args)
-    @flag = args[:flag]
-  end
-
-  def local_spares
-    {flag: flag}
-  end
-
-  def default_chain
-    '9-speed'
-  end
-
-  def default_tire_size
-    '28'
-  end
-end
+chain = Part.new(name: 'chain', description: '10-speed')
+road_tire = Part.new(name: 'tire_size', description: '23')
+mountain_tire = Part.new(name: 'tire_size', description: '2.1')
+red_tape = Part.new(name: 'tape_color', description: 'red')
+rear_shock = Part.new(name: 'rear_shock', description: 'Fox')
+front_shock = Part.new(name: 'front_shock', description: 'Manitou', needs_spare: false)
