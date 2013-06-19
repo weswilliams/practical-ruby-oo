c31a925902dbc1078fc17761aed8fa5b6effae1d
Wes Williams <wes.williamz@gmail.com>
2013-06-18 20:07:12 -0700

    bicycle configs

--- Bicycle.rb
+++ Bicycle.rb
@@ -52,3 +52,8 @@ mountain_config = [
   ['front_shock', 'Manitou', false],
   ['rear_shock', 'Fox']]
 
+recumbent_config = [
+  ['chain', '9-speed'],
+  ['tire_size', '28'],
+  ['flag', 'tall and orange']]
+
